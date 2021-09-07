d9b424a8712d7da8b1b98ba2e9f6b51678b71a00
Murillo R. Araujo <mr_araujo@icloud.com>
2021-09-07 17:54:21 -0300

    Initial Commit

---
+++ Shared/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  Shared
+//
+//  Created by Murillo R. Araujo on 07/09/21.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
