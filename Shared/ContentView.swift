a96311943917ae712c25a58f3b12d3494cf2c6ab
Murillo R. Araujo <mr_araujo@icloud.com>
2021-09-07 18:18:50 -0300

    Section 17 (lecture-214):  Understanding the List and Identifiable Protocol

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("H4X0S News")
+        }
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Hello"),
+    Post(id: "3", title: "Hello"),
+    Post(id: "4", title: "Hello"),
+    Post(id: "5", title: "Hello"),
+    Post(id: "6", title: "Hello"),
+    Post(id: "7", title: "Hello")
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
